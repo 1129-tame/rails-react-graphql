d1c2e18f503caa6604a3510874cc70542ecc861e
haruka <onemanlive0603tame@yahoo.ne.jp>
2022-09-24 04:56:00 +0000

    mutation の作成

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,9 @@
 module Types
   class MutationType < Types::BaseObject
+    field :create_book, mutation: Mutations::CreateBook
+    field :update_book, mutation: Mutations::UpdateBook
+    field :delete_book, mutation: Mutations::DeleteBook
+    
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
